1baf1bf515f53619b545cf8728d91436c4dda829
sergiojmn <146011336+sergiojmn@users.noreply.github.com>
2026-04-13 21:11:58 +0000

    deploy 2

--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import useLocalStorage from "./hooks/useLocalStorage";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 /* =========================
    DATA ORIGINAL
